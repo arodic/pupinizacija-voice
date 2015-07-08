b1368f8e7d28dd5a47f54af3fb7ac55f7053565c
Aki Rodic <aleksandar.xyz@gmail.com>
2015-07-08 14:55:52 +0200

    Prvi prototip i bild.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# pupinizacija-voice
+# Installation
+
+Install node.js 0.12.x or higher
+
+```bash
+npm install -g gulp bower
+npm install && bower install
+
+```
+
+#serving
+
+gulp serve
---
+++ app/scripts/app.js
@@ -0,0 +1,51 @@
+(function(document) {
+
+  'use strict';
+
+  var app = document.querySelector('#app');
+  app.addEventListener('dom-change', function() {
+
+    var voice = document.querySelector('#webspeech');
+    var running = false;
+
+    // TODO: Dodaj ostale reci
+    var upitnereci = ['kako', 'gde', 'zašto', 'kada', 'koliko', 'da li', 'ko je', 'ko su'];
+
+    var startSpeech = function() {
+      if (running) {
+        return;
+      }
+      voice.recognition.lang = 'sr';
+      voice.start();
+    };
+
+    // var endSpeech = function() {
+    //   voice.stop();
+    // };
+
+    document.addEventListener('keydown', startSpeech);
+    document.addEventListener('mousedown', startSpeech);
+    // document.addEventListener('keyup', endSpeech);
+    // document.addEventListener('mouseup', endSpeech);
+
+    app.onResult = function(event) {
+      console.log(voice.recognition);
+      console.log(event.detail.result);
+      this.result = event.detail.result;
+      for (var i = 0; i < upitnereci.length; i++) {
+        if (this.result.search(upitnereci[i]) === 0) {
+          this.result += '?';
+        }
+      }
+      voice.text = '';
+    };
+    app.onStart = function() {
+      running = true;
+    };
+    app.onEnd = function() {
+      running = false;
+    };
+
+  });
+
+})(document);
