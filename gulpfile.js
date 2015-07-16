ea9145197f57acaa2b7b036524d05f34bb5990d0
Aki Rodic <aleksandar.xyz@gmail.com>
2015-07-16 15:09:49 +0200

    build

--- gulpfile.js
+++ gulpfile.js
@@ -74,10 +74,10 @@ gulp.task('copy', function () {
     .pipe(gulp.dest('dist/elements'));
 
   var elements = gulp.src(['app/scripts/**/*.js'])
-    .pipe(gulp.dest('dist/scripts'));
+    .pipe(gulp.dest('dist/scripts'));;
 
-  var elements = gulp.src(['app/scripts/**/*.json'])
-    .pipe(gulp.dest('dist/scripts'));
+  var images = gulp.src(['app/images/**/*.*'])
+    .pipe(gulp.dest('dist/images'));
 
   var swBootstrap = gulp.src(['bower_components/platinum-sw/bootstrap/*.js'])
     .pipe(gulp.dest('dist/elements/bootstrap'));
@@ -89,7 +89,7 @@ gulp.task('copy', function () {
     .pipe($.rename('elements.vulcanized.html'))
     .pipe(gulp.dest('dist/elements'));
 
-  return merge(app, bower, elements, vulcanized, swBootstrap, swToolbox)
+  return merge(app, bower, elements, images, vulcanized, swBootstrap, swToolbox)
     .pipe($.size({title: 'copy'}));
 });
 
